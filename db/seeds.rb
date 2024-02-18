053cee3b45ddf73048d16e10263efeceaef3e278
takumi_Mac <>
2024-02-18 23:13:11 +0900

    rack-cols追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+10.times do |i|
+    Book.create(title: "ダミーtitle#{i}", body: "ダミーbody#{i}")
+end
